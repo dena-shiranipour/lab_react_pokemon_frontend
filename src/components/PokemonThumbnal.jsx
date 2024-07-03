d3c6a09d1179fcd20f21af55e1477a06c0a2c4ea
Rabin Kandel <kandelrabin@live.com>
2024-07-03 16:16:41 +0100

    write fetchAllPokemons and render pokemon Thumbnails

--- src/components/PokemonThumbnal.jsx
+++ src/components/PokemonThumbnal.jsx
@@ -1,7 +1,20 @@
-const PokemonThumbnail = () => {
+const PokemonThumbnail = ({pokemons}) => {
+  
   return(
     <>
-      <h4>Hello from Pokemon</h4>
+      <section className="pokemonThumbnail">
+        {
+          pokemons.map((pokemon, index) => {
+            return(
+            <div key={index}>
+                <h3>{pokemon.forms[0].name}</h3>
+                <img src={pokemon.sprites.front_default} alt={pokemon.forms[0].name} />
+            </div>
+            )
+          })
+        }
+
+      </section>
     </>
   )
 }
--- src/containers/PokemonContainer.jsx
+++ src/containers/PokemonContainer.jsx
@@ -1,16 +1,36 @@
-import { useState } from "react"
+import { useState, useEffect} from "react"
 import PokemonThumbnail from "../components/PokemonThumbnal"
 
 const PokemonContainer = () => {
 
-  const [pokemons, setPokemon] = useState([])
+  const [pokemons, setPokemons] = useState([]);
 
+  const fetchAllPokemonsURLS = async () => {
+    const response = await fetch ("https://pokeapi.co/api/v2/pokemon?limit=500");
+    const data = await response.json();
+    return data;
+  }
+
+  const fetchAllPokemons = async () => {
+    const pokemonsURLS = await fetchAllPokemonsURLS();
+    console.log("results :"+ pokemonsURLS.results);
+    const fetchPromises = pokemonsURLS.results.map((element)=> fetch(element.url));
+    const responses = await Promise.all(fetchPromises);
+    const jsonPromises = responses.map((element)=>element.json());
+    const pokemonData = await Promise.all(jsonPromises);
+    setPokemons(pokemonData);
+  }
+
+  useEffect(()=>{
+    fetchAllPokemons()
+  },[])
   
+  console.log(pokemons);
 
   return(
     <>
       <h2>Hello from PokemonContainer</h2>
-      <PokemonThumbnail />
+      {pokemons ? <PokemonThumbnail pokemons={pokemons}/> : "Fetching pokemons..."}
     </>
   )
 }
